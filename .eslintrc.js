ecbc81fdbec5d9d3d4a5a0402a8b6abaa3b61465
Scott Cooper <scttcper@gmail.com>
2019-06-30 22:12:33 -0400

    fix: update dep switch to eslint

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+    jest: true,
+  },
+  extends: ['xo-space/esnext', 'xo-typescript', 'prettier/@typescript-eslint'],
+  rules: {
+    'object-curly-spacing': ['error', 'always'],
+    '@typescript-eslint/indent': ['error', 2, { SwitchCase: 1 }],
+    '@typescript-eslint/explicit-function-return-type': 0,
+    'capitalized-comments': 0,
+    'comma-dangle': ['error', 'always-multiline'],
+    '@typescript-eslint/camelcase': 0,
+    quotes: 0,
+    'quote-props': 0,
+    'no-script-url': 0,
+  },
+};
